2050ebc2d1fe052dabc8541019c969609eabb1c2
Boucher-David <david.boucher@ookla.com>
2017-09-19 20:28:25 -0700

    Update greet.test.js

--- lab-david/test/greet.test.js
+++ lab-david/test/greet.test.js
@@ -7,10 +7,14 @@ var assert = require('assert');
 describe('Greet', () => {
 
     it('should return null if user does not enter string', () => {
+        
         assert.equal(null, greet(42));
+        
     });
 
     it('should return Hello World', () => {
+        
         assert.equal("Hello World", greet("World"));
+        
     });
 });
